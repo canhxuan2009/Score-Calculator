006d1b8df38cc38f26c799ce330ee1b99b7695bd
canhxuan2009 <xuanmc2009@gmail.com>
2026-08-06 23:18:33 +0700

    feat(parser): add deterministic event semantics

--- src/point_audit/parsing/__init__.py
+++ src/point_audit/parsing/__init__.py
@@ -8,12 +8,14 @@ from point_audit.parsing.segmenter import (
     segment_evidence,
     starts_new_event,
 )
+from point_audit.parsing.semantic_parser import parse_event_candidate
 
 __all__ = [
     "EventSegment",
     "SegmentationResult",
     "event_candidates_from_segments",
     "is_decimal_comma",
+    "parse_event_candidate",
     "segment_evidence",
     "starts_new_event",
 ]
